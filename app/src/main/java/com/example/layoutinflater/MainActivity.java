d05e858b3b18065001cfe7e7efb9435fd89b418e
FeesTheFox <feesblock@gmail.com>
2024-03-12 17:20:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/layoutinflater/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.layoutinflater;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import com.example.layoutinflater.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    final String LOG_TAG = "myLogs";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LayoutInflater ltInflater = getLayoutInflater();
+        View view = ltInflater.inflate(R.layout.text, null, false);
+
+        LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
+        linLayout.addView(view);
+
+        LinearLayout relLayout = (LinearLayout) findViewById(R.id.rellayout);
+        View view2 = ltInflater.inflate(R.layout.text,relLayout,true);
+
+        Log.d(LOG_TAG, "Class of view: " + view.getClass().toString());
+        Log.d(LOG_TAG, "Class of view: " + view2.getClass().toString());
+    }
+}
---
+++ app/src/main/java/com/example/layoutinflater/text.java
@@ -0,0 +1,14 @@
+package com.example.layoutinflater;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class text extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text);
+    }
+}
